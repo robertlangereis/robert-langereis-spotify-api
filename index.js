10032ea29aa34c9913017e5351588a288846aaf9
robertlangereis <robert.langereis@gmail.com>
2019-04-20 11:22:10 +0200

    complete restart initial setup

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const songsRouter = require('./songs/routes')
+const playlistsRouter = require('./playlists/routes')
+const authenticationRouter = require('./auth/routes')
+const usersRouter = require('./users/routes')
+
+const app = express()
+const port = process.env.PORT || 4000
+
+app
+  .use(bodyParser.json())
+  .use(songsRouter, playlistsRouter, authenticationRouter, usersRouter)
+  .listen(port, () => console.log(`Listening on port ${port}`))
